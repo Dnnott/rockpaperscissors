eb51a8bb13e080b460cc4c380d1a11b9bd52f043
DanVR <daniel.vinues@gmail.com>
2022-07-21 13:59:07 +0200

    Add index.html myscript.js mystyle.css

---
+++ myscript.js
@@ -0,0 +1,50 @@
+function getComputerChoice(){
+    eleccion = Math.floor(Math.random() * 3);
+    if (eleccion == 0){
+        return "rock";
+        console.log("Computer selects: Rock")
+    }
+    else if (eleccion == 1){
+        return "paper";
+        console.log("Computer selects: Paper")
+    }
+    else{
+        return "scissors";
+        console.log("Computer selects: Scissors")
+    }
+}
+
+function playRound(playerSelection,computerSelection){
+    //* Comprobación
+    playerSelection.toLowerCase();
+        if (playerSelection != "rock" || "scissors" || "paper"){
+          console.log("Please select Rock, Paper or Scissors: ")
+          return ""
+         }
+
+    //* Comparaciones computer win
+    if (computerSelection == "rock" & playerSelection == "scissors"){
+        console.log("You Lose! Rock beats Scissors");
+        return "Computer Wins"
+    }
+    else if (computerSelection == "scissors" & playerSelection == "paper"){
+        console.log("You Lose! Scissors beats Paper");
+        return "Computer Wins"
+    }
+    else if (computerSelection == "paper" & playerSelection == "scissors"){
+        console.log("You Win! Scissors beats Paper");
+        return "Player Wins"
+    }
+    else if (computerSelection == "Scissors" & playerSelection == "rock"){
+        console.log("You Win! Rock beats Scissors");
+        return "Player Wins"
+    }
+}
+        
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    
+}
