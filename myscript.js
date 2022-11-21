568b97c9d2bddeed937b7fcbcdf10d8e2ee212ca
DanVR <daniel.vinues@gmail.com>
2022-11-21 08:54:57 +0100

    Add index.html

--- myscript.js
+++ myscript.js
@@ -72,10 +72,14 @@ function game(){
      
 }
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-  alert("Hello World");
-});
+//const btn = document.querySelector('#btn');
+//btn.addEventListener('click', () => {
+//  alert("Hello World");
+//});
+
+btn.addEventListener('click', function (e) {
+    console.log(e);
+  });
 
 console.log(game());
 
